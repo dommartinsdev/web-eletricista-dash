2348702b13639f51e89ee9e864a76b211543ab52
Jessé Martins <jessemartins.ti@gmail.com>
2022-06-29 09:54:24 -0300

    feat:finalização da página serviços do projeto

--- src/app/views/service/service.component.ts
+++ src/app/views/service/service.component.ts
@@ -6,9 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./service.component.css']
 })
 export class ServiceComponent implements OnInit {
-
+  services = [
+    { 
+      title: 'Reparação de Quadros',
+      description:'Clique aqui e comece a digitar. Veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem.'
+    },
+    { 
+      title: 'Inspecção de Fiação',
+      description:'Clique aqui e comece a digitar. Veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem.'
+    },
+    { 
+      title: 'Serviços de Emergência',
+      description:'Clique aqui e comece a digitar. Veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem.'
+    },
+    { 
+      title: 'Instalações Eléctricas',
+      description:'Clique aqui e comece a digitar. Veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem.'
+    },
+    { 
+      title: 'Reparação de Tomadas',
+      description:'Clique aqui e comece a digitar. Veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem.'
+    },
+    { 
+      title: 'Manutenção Geral',
+      description:'Clique aqui e comece a digitar. Veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem.'
+    }
+  ]
   constructor() { }
-
   ngOnInit(): void {
   }
 
